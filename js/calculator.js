9d8ea86cf42fd10fc195a37aa4e733b256eace45
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-15 19:45:08 +0530

    Inital commit

---
+++ js/calculator.js
@@ -0,0 +1,20 @@
+
+class Calculator {
+    
+    Add(a, b) {
+        return (a+b);
+    }
+    
+    Sub(a, b) {
+        return (a-b);
+    }
+    
+    Mul(a, b) {
+        return (a*b);
+    }
+    
+    Div(a, b) {
+        return (a/b);
+    }
+
+}
