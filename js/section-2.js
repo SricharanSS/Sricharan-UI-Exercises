0d5c288f140c03f3cfaa222fa319e3065e65a8d9
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-28 18:18:33 +0530

    Change for to for-of loop

--- js/section-2.js
+++ js/section-2.js
@@ -1,5 +1,5 @@
 // Insert 11 players
-playerList = ["Sricharan","Hari Haran", "Vasanth", "Poorna", "James", "Mukund", "Gokul", "Abdul", "Hari Booba", "Charan Raj", "Kumeresh"];
+let playerList = ["Sricharan","Hari Haran", "Vasanth", "Poorna", "James", "Mukund", "Gokul", "Abdul", "Hari Booba", "Charan Raj", "Kumeresh"];
 console.log(playerList);
 
 // Remove First player
@@ -16,14 +16,14 @@ console.log(playerList);
 playerList.sort();
 
 // Assign random numbers to players
-for(let i=0; i<playerList.length; i++) {
-    console.log(playerList[i]+" : "+Math.floor((Math.random()*100)+1));
+for(const element of playerList) {
+    console.log(element+" : "+Math.floor((Math.random()*100)+1));
 }
 
 // Store player name and jersey number
 let playerMap = {};
-for(let i=0; i<playerList.length; i++) {
-    playerMap[playerList[i].toUpperCase()] = Math.floor((Math.random() * 100) + 1);
+for(const element of playerList) {
+    playerMap[element.toUpperCase()] = Math.floor((Math.random() * 100) + 1);
 }
 
 console.log(playerMap);
