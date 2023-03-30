7e659bf5a896285622c282ccb81518c51bfe5ac8
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-30 18:42:59 +0530

    Add template Litrals and use ===

--- js/index.js
+++ js/index.js
@@ -11,51 +11,51 @@ fetch('../assets/BankDetails.json').then(res => res.json()).then(BankDetails => 
     });
     
     document.getElementById("btn-atm-submit").addEventListener("click", () => {
-        function atmWithDraw(Card_No, pin, amount) {
+        function atmWithDraw(cardNo, pin, amount) {
             let flag = 0;
             for(const element of BankDetails) {
-                if( (element.Card_No == cardNo) && (element.pin == pin) ) {
+                if( ( parseInt(element.Card_No) === cardNo ) && ( parseInt(element.pin) === pin ) ) {
                     flag = 1;
                     if(amount <= element.Account_Balance) {
                         element.Account_Balance -= amount;
-                        alert("Amount Withdrawn : "+amount+"\nBalance : "+element.Account_Balance);
+                        alert(`Amount Withdrawn : `+amount+`\nBalance : `+element.Account_Balance);
                     }
                     else {
-                        alert("Insufficiant Balance");
+                        alert(`Insufficiant Balance`);
                     }
                 }
             }
     
-            if(flag == 0) {
-                alert("Card Number or PIN Wrong");
+            if(flag === 0) {
+                alert(`Card Number or PIN Wrong`);
             }
         }
-        let cardNo = document.getElementById("atm-card-no").value;
-        let pin = document.getElementById("atm-pin").value;
-        let amount = document.getElementById("atm-amount").value;
+        let cardNo = parseInt(document.getElementById("atm-card-no").value);
+        let pin = parseInt(document.getElementById("atm-pin").value);
+        let amount = parseInt(document.getElementById("atm-amount").value);
     
         atmWithDraw(cardNo, pin, amount);
     
     });
     
     document.getElementById("btn-cdm-submit").addEventListener("click", () => {
-        function cdmDeposit(Card_No, pin, amount) {
+        function cdmDeposit(cardNo, pin, amount) {
             let flag = 0;
             for(const element of BankDetails) {
-                if(element.Card_No == cardNo && element.pin == pin) {
+                if( ( parseInt(element.Card_No) === cardNo ) && ( parseInt(element.pin) === pin ) ) {
                     flag = 1;
                     element.Account_Balance += parseInt(amount);
-                    alert("Amount Deposited : "+amount+"\nBalance : "+element.Account_Balance);
+                    alert(`Amount Deposited : `+amount+`\nBalance : `+element.Account_Balance);
                 }
             }
     
-            if(flag == 0) {
-                alert("Card Number or PIN Wrong");
+            if(flag === 0) {
+                alert(`Card Number or PIN Wrong`);
             }
         }
-        let cardNo = document.getElementById("cdm-card-no").value;
-        let pin = document.getElementById("cdm-pin").value;
-        let amount = document.getElementById("cdm-amount").value;
+        let cardNo = parseInt(document.getElementById("cdm-card-no").value);
+        let pin = parseInt(document.getElementById("cdm-pin").value);
+        let amount = parseInt(document.getElementById("cdm-amount").value);
     
         cdmDeposit(cardNo, pin, amount);
     });
